f3625d43fea7e0c7cd188a7d7a1e619bf58063b2
Steven Flintham <sgf@lemma.co.uk>
2019-07-12 17:46:19 +0100

    Apply 016_RainfallOptionsChangeSettings.patch

--- src/rainfall_options_gui.cpp
+++ src/rainfall_options_gui.cpp
@@ -10,6 +10,8 @@
 /** @file rainfall_option_gui.cpp Expert GUI for configuring the rainfall river generator */
 
 #include "stdafx.h"
+#include "querystring_gui.h"
+#include "rivers_rainfall.h"
 #include "settings_type.h"
 #include "strings_func.h"
 #include "string_func.h"
@@ -35,6 +37,8 @@ static const StringID _widen_valleys_options[]   = {STR_RAINFALL_WIDER_VALLEYS_O
  *  It can be opened both from the world generation, and from the heightmap generation dialog.
  */
 struct RainfallOptionWindow : Window {
+
+	uint query_widget_id;
 
 	RainfallOptionWindow(WindowDesc *desc, Window *parent, WindowNumber window_number = 0) : Window(desc)
 	{
@@ -101,6 +105,290 @@ struct RainfallOptionWindow : Window {
 			case WID_RFO_SHORE_MAX_SIZE_TEXT:
 				SetDParam(0, 10000);
 				*size = GetStringBoundingBox(STR_JUST_INT);
+				break;
+		}
+	}
+
+	virtual void OnTimeout()
+	{
+		static const int raise_widgets[] = {
+			WID_RFO_FLOW_FOR_RIVER_DOWN, WID_RFO_FLOW_FOR_RIVER_UP,
+			WID_RFO_LAKE_VOLUME_DOWN, WID_RFO_LAKE_VOLUME_UP,
+			WID_RFO_FLOW_MODIFICATIONS_DOWN, WID_RFO_FLOW_MODIFICATIONS_UP,
+			WID_RFO_WIDER_RIVERS_MULT_DOWN, WID_RFO_WIDER_RIVERS_MULT_UP,
+			WID_RFO_WIDER_VALLEYS_MULT_DOWN, WID_RFO_WIDER_VALLEYS_MULT_UP,
+			WID_RFO_WIDER_VALLEYS_RANDOM_DOWN, WID_RFO_WIDER_VALLEYS_RANDOM_UP,
+			WID_RFO_OUTFLOW_CANYON_PROBABILITY_DOWN, WID_RFO_OUTFLOW_CANYON_PROBABILITY_UP,
+			WID_RFO_MINIMIZE_LAKE_PROBABILITY_DOWN, WID_RFO_MINIMIZE_LAKE_PROBABILITY_UP,
+			WID_RFO_ISLAND_PROBABILITY_DOWN, WID_RFO_ISLAND_PROBABILITY_UP,
+			WID_RFO_ISLAND_MAX_SIZE_DOWN, WID_RFO_ISLAND_MAX_SIZE_UP,
+			WID_RFO_SHORE_PROBABILITY_DOWN, WID_RFO_SHORE_PROBABILITY_UP,
+			WID_RFO_SHORE_MAX_SIZE_DOWN, WID_RFO_SHORE_MAX_SIZE_UP,
+			WIDGET_LIST_END };
+
+		for (const int *widget = raise_widgets; *widget != WIDGET_LIST_END; widget++) {
+			if (this->IsWidgetLowered(*widget)) {
+				this->RaiseWidget(*widget);
+				this->SetWidgetDirty(*widget);
+			}
+		}
+	}
+
+	/** Processes one click to a spinner widet.
+     *  @param widget the widget
+     *  @param ref_widget the corresponding Text widget
+     *  @param config_setting the config setting controlled by the spinner, will be changed.
+     *  @param min_value minimum allowed value for the config setting
+     *  @param max_value maximum allowed value for the config setting
+     */
+	template<typename T>
+	void ProcessSpinnerClick(int widget, int ref_widget, T &config_setting, T min_value, T max_value)
+	{
+		/* Don't allow too fast scrolling */
+		if (!(this->flags & WF_TIMEOUT) || this->timeout_timer <= 1) {
+			this->HandleButtonClick(widget);
+			this->SetDirty();
+
+			config_setting = Clamp(config_setting + widget - ref_widget, min_value, max_value);
+		}
+		_left_button_clicked = false;
+	}
+
+	virtual void OnClick(Point pt, int widget, int click_count)
+	{
+		switch (widget) {
+			case WID_RFO_FLOW_FOR_RIVER_TEXT:
+				this->query_widget_id = WID_RFO_FLOW_FOR_RIVER_TEXT;
+				SetDParam(0, _settings_newgame.game_creation.rainfall.flow_for_river);
+				ShowQueryString(STR_JUST_INT, STR_RAINFALL_FLOW_FOR_RIVER_QUERY_CAPT, 8, this, CS_NUMERAL, QSF_ENABLE_DEFAULT);
+				break;
+
+			case WID_RFO_FLOW_FOR_RIVER_DOWN:
+			case WID_RFO_FLOW_FOR_RIVER_UP:
+				this->ProcessSpinnerClick<int32>(widget, WID_RFO_FLOW_FOR_RIVER_TEXT, _settings_newgame.game_creation.rainfall.flow_for_river, MIN_FLOW_FOR_RIVER, MAX_FLOW_FOR_RIVER);
+				break;
+
+			case WID_RFO_LAKE_VOLUME_TEXT:
+				this->query_widget_id = WID_RFO_LAKE_VOLUME_TEXT;
+				SetDParam(0, _settings_newgame.game_creation.rainfall.flow_per_lake_volume);
+				ShowQueryString(STR_JUST_INT, STR_RAINFALL_FLOW_PER_LAKE_VOLUME_QUERY_CAPT, 8, this, CS_NUMERAL, QSF_ENABLE_DEFAULT);
+				break;
+
+			case WID_RFO_LAKE_VOLUME_DOWN:
+			case WID_RFO_LAKE_VOLUME_UP:
+				this->ProcessSpinnerClick<int32>(widget, WID_RFO_LAKE_VOLUME_TEXT, _settings_newgame.game_creation.rainfall.flow_per_lake_volume, MIN_FLOW_PER_LAKE_VOLUME, MAX_FLOW_PER_LAKE_VOLUME);
+				break;
+
+			case WID_RFO_FLOW_MODIFICATIONS_TEXT:
+				this->query_widget_id = WID_RFO_FLOW_MODIFICATIONS_TEXT;
+				SetDParam(0, _settings_newgame.game_creation.rainfall.number_of_flow_modifications);
+				ShowQueryString(STR_JUST_INT, STR_RAINFALL_FLOW_MODIFICATION_QUERY_CAPT, 6, this, CS_NUMERAL, QSF_ENABLE_DEFAULT);
+				break;
+
+			case WID_RFO_FLOW_MODIFICATIONS_DOWN:
+			case WID_RFO_FLOW_MODIFICATIONS_UP:
+				this->ProcessSpinnerClick<int16>(widget, WID_RFO_FLOW_MODIFICATIONS_TEXT, _settings_newgame.game_creation.rainfall.number_of_flow_modifications,
+								  MIN_NUMBER_OF_FLOW_MODIFICATIONS, MAX_NUMBER_OF_FLOW_MODIFICATIONS);
+				break;
+
+			case WID_RFO_WIDER_RIVERS_DROPDOWN:
+				ShowDropDownMenu(this, _widen_rivers_options, _settings_newgame.game_creation.rainfall.wider_rivers_enabled, WID_RFO_WIDER_RIVERS_DROPDOWN, 0, 0);
+				break;
+
+			case WID_RFO_WIDER_RIVERS_MULT_TEXT:
+				this->query_widget_id = WID_RFO_WIDER_RIVERS_MULT_TEXT;
+				SetDParam(0, _settings_newgame.game_creation.rainfall.wider_rivers_multiplier);
+				ShowQueryString(STR_JUST_INT, STR_RAINFALL_RIVER_EXPANSION_MULTIPLIER_QUERY_CAPT, 6, this, CS_NUMERAL, QSF_ENABLE_DEFAULT);
+				break;
+
+			case WID_RFO_WIDER_RIVERS_MULT_DOWN:
+			case WID_RFO_WIDER_RIVERS_MULT_UP:
+				this->ProcessSpinnerClick<int16>(widget, WID_RFO_WIDER_RIVERS_MULT_TEXT, _settings_newgame.game_creation.rainfall.wider_rivers_multiplier,
+								  MIN_WIDER_RIVERS_MULTIPLIER, MAX_WIDER_RIVERS_MULTIPLIER);
+				break;
+
+			case WID_RFO_WIDER_VALLEYS_DROPDOWN:
+				ShowDropDownMenu(this, _widen_valleys_options, _settings_newgame.game_creation.rainfall.wider_valleys_enabled, WID_RFO_WIDER_VALLEYS_DROPDOWN, 0, 0);
+				break;
+
+			case WID_RFO_WIDER_VALLEYS_MULT_TEXT:
+				this->query_widget_id = WID_RFO_WIDER_VALLEYS_MULT_TEXT;
+				SetDParam(0, _settings_newgame.game_creation.rainfall.wider_valleys_multiplier);
+				ShowQueryString(STR_JUST_INT, STR_RAINFALL_WIDER_VALLEYS_MULTIPLIER_QUERY_CAPT, 6, this, CS_NUMERAL, QSF_ENABLE_DEFAULT);
+				break;
+
+			case WID_RFO_WIDER_VALLEYS_MULT_DOWN:
+			case WID_RFO_WIDER_VALLEYS_MULT_UP:
+				this->ProcessSpinnerClick<int16>(widget, WID_RFO_WIDER_VALLEYS_MULT_TEXT, _settings_newgame.game_creation.rainfall.wider_valleys_multiplier,
+								  MIN_WIDER_VALLEYS_MULTIPLIER, MAX_WIDER_VALLEYS_MULTIPLIER);
+				break;
+
+			case WID_RFO_WIDER_VALLEYS_RANDOM_TEXT:
+				this->query_widget_id = WID_RFO_WIDER_VALLEYS_RANDOM_TEXT;
+				SetDParam(0, _settings_newgame.game_creation.rainfall.wider_valleys_randomness);
+				ShowQueryString(STR_JUST_INT, STR_RAINFALL_WIDER_VALLEYS_RANDOMNESS_QUERY_CAPT, 6, this, CS_NUMERAL, QSF_ENABLE_DEFAULT);
+				break;
+
+			case WID_RFO_WIDER_VALLEYS_RANDOM_DOWN:
+			case WID_RFO_WIDER_VALLEYS_RANDOM_UP:
+				this->ProcessSpinnerClick<int16>(widget, WID_RFO_WIDER_VALLEYS_RANDOM_TEXT, _settings_newgame.game_creation.rainfall.wider_valleys_randomness,
+								  MIN_WIDER_VALLEYS_RANDOMNESS, MAX_WIDER_VALLEYS_RANDOMNESS);
+				break;
+
+			case WID_RFO_OUTFLOW_CANYON_PROBABILITY_TEXT:
+				this->query_widget_id = WID_RFO_OUTFLOW_CANYON_PROBABILITY_TEXT;
+				SetDParam(0, _settings_newgame.game_creation.rainfall.lake_outflow_canyon_probability);
+				ShowQueryString(STR_JUST_INT, STR_RAINFALL_LAKE_OUTFLOW_CANYON_PROBABILITY_QUERY_CAPT, 5, this, CS_NUMERAL, QSF_ENABLE_DEFAULT);
+				break;
+
+			case WID_RFO_OUTFLOW_CANYON_PROBABILITY_DOWN:
+			case WID_RFO_OUTFLOW_CANYON_PROBABILITY_UP:
+				this->ProcessSpinnerClick<uint16>(widget, WID_RFO_OUTFLOW_CANYON_PROBABILITY_TEXT, _settings_newgame.game_creation.rainfall.lake_outflow_canyon_probability,
+								  MIN_RAINFALL_PROBABILITY, MAX_RAINFALL_PROBABILITY);
+				break;
+
+			case WID_RFO_MINIMIZE_LAKE_PROBABILITY_TEXT:
+				this->query_widget_id = WID_RFO_MINIMIZE_LAKE_PROBABILITY_TEXT;
+				SetDParam(0, _settings_newgame.game_creation.rainfall.lake_reduce_to_guaranteed_probability);
+				ShowQueryString(STR_JUST_INT, STR_RAINFALL_LAKE_MINIMIZE_PROBABILITY_QUERY_CAPT, 5, this, CS_NUMERAL, QSF_ENABLE_DEFAULT);
+				break;
+
+			case WID_RFO_MINIMIZE_LAKE_PROBABILITY_DOWN:
+			case WID_RFO_MINIMIZE_LAKE_PROBABILITY_UP:
+				this->ProcessSpinnerClick<uint16>(widget, WID_RFO_MINIMIZE_LAKE_PROBABILITY_TEXT, _settings_newgame.game_creation.rainfall.lake_reduce_to_guaranteed_probability,
+								  MIN_RAINFALL_PROBABILITY, MAX_RAINFALL_PROBABILITY);
+				break;
+
+			case WID_RFO_ISLAND_PROBABILITY_TEXT:
+				this->query_widget_id = WID_RFO_ISLAND_PROBABILITY_TEXT;
+				SetDParam(0, _settings_newgame.game_creation.rainfall.lake_island_probability);
+				ShowQueryString(STR_JUST_INT, STR_RAINFALL_LAKE_ISLAND_PROBABILITY_QUERY_CAPT, 5, this, CS_NUMERAL, QSF_ENABLE_DEFAULT);
+				break;
+
+			case WID_RFO_ISLAND_PROBABILITY_DOWN:
+			case WID_RFO_ISLAND_PROBABILITY_UP:
+				this->ProcessSpinnerClick<uint16>(widget, WID_RFO_ISLAND_PROBABILITY_TEXT, _settings_newgame.game_creation.rainfall.lake_island_probability,
+									MIN_RAINFALL_PROBABILITY, MAX_RAINFALL_PROBABILITY);
+				break;
+
+			case WID_RFO_ISLAND_MAX_SIZE_TEXT:
+				this->query_widget_id = WID_RFO_ISLAND_MAX_SIZE_TEXT;
+				SetDParam(0, _settings_newgame.game_creation.rainfall.lake_island_max_size);
+				ShowQueryString(STR_JUST_INT, STR_RAINFALL_LAKE_ISLAND_MAX_SIZE_QUERY_CAPT, 6, this, CS_NUMERAL, QSF_ENABLE_DEFAULT);
+				break;
+
+			case WID_RFO_ISLAND_MAX_SIZE_DOWN:
+			case WID_RFO_ISLAND_MAX_SIZE_UP:
+				this->ProcessSpinnerClick<uint16>(widget, WID_RFO_ISLAND_MAX_SIZE_TEXT, _settings_newgame.game_creation.rainfall.lake_island_max_size, MIN_RAINFALL_MAX_SIZE, MAX_RAINFALL_MAX_SIZE);
+				break;
+
+			case WID_RFO_SHORE_PROBABILITY_TEXT:
+				this->query_widget_id = WID_RFO_SHORE_PROBABILITY_TEXT;
+				SetDParam(0, _settings_newgame.game_creation.rainfall.lake_shore_probability);
+				ShowQueryString(STR_JUST_INT, STR_RAINFALL_LAKE_MORE_LAND_AT_SHORE_PROBABILITY_QUERY_CAPT, 5, this, CS_NUMERAL, QSF_ENABLE_DEFAULT);
+				break;
+
+			case WID_RFO_SHORE_PROBABILITY_DOWN:
+			case WID_RFO_SHORE_PROBABILITY_UP:
+				this->ProcessSpinnerClick<uint16>(widget, WID_RFO_SHORE_PROBABILITY_TEXT, _settings_newgame.game_creation.rainfall.lake_shore_probability,
+									  MIN_RAINFALL_PROBABILITY, MAX_RAINFALL_PROBABILITY);
+				break;
+
+			case WID_RFO_SHORE_MAX_SIZE_TEXT:
+				this->query_widget_id = WID_RFO_SHORE_MAX_SIZE_TEXT;
+				SetDParam(0, _settings_newgame.game_creation.rainfall.lake_shore_max_size);
+				ShowQueryString(STR_JUST_INT, STR_RAINFALL_LAKE_SHORE_MAX_SIZE_QUERY_CAPT, 6, this, CS_NUMERAL, QSF_ENABLE_DEFAULT);
+				break;
+
+			case WID_RFO_SHORE_MAX_SIZE_DOWN:
+			case WID_RFO_SHORE_MAX_SIZE_UP:
+				this->ProcessSpinnerClick<uint16>(widget, WID_RFO_SHORE_MAX_SIZE_TEXT, _settings_newgame.game_creation.rainfall.lake_shore_max_size, MIN_RAINFALL_MAX_SIZE, MAX_RAINFALL_MAX_SIZE);
+				break;
+		}
+	}
+
+	virtual void OnDropdownSelect(int widget, int index)
+	{
+		switch (widget) {
+			case WID_RFO_WIDER_RIVERS_DROPDOWN:
+				_settings_newgame.game_creation.rainfall.wider_rivers_enabled = index;
+			break;
+			case WID_RFO_WIDER_VALLEYS_DROPDOWN:
+				_settings_newgame.game_creation.rainfall.wider_valleys_enabled = index;
+			break;
+		}
+		this->InvalidateData();
+	}
+
+	virtual void OnQueryTextFinished(char *str)
+	{
+		/* Was 'cancel' pressed? */
+		if (str == NULL) return;
+
+		int32 value;
+		if (!StrEmpty(str)) {
+			value = atoi(str);
+		} else {
+			/* An empty string means revert to the default */
+			switch (this->query_widget_id) {
+				case WID_RFO_FLOW_FOR_RIVER_TEXT:              value = DEF_MODERATE_RIVERS_FLOW_FOR_RIVER; break;
+				case WID_RFO_LAKE_VOLUME_TEXT:                 value = DEF_MODERATE_RIVERS_FLOW_PER_LAKE_VOLUME; break;
+				case WID_RFO_FLOW_MODIFICATIONS_TEXT:          value = DEF_NUMBER_OF_FLOW_MODIFICATIONS; break;
+				case WID_RFO_WIDER_RIVERS_MULT_TEXT:           value = DEF_WIDER_RIVERS_MULTIPLIER; break;
+				case WID_RFO_WIDER_VALLEYS_MULT_TEXT:          value = DEF_WIDER_VALLEYS_MULTIPLIER; break;
+				case WID_RFO_WIDER_VALLEYS_RANDOM_TEXT:        value = DEF_WIDER_VALLEYS_RANDOMNESS; break;
+
+				case WID_RFO_OUTFLOW_CANYON_PROBABILITY_TEXT:  value = DEF_LAKE_OUTFLOW_CANYON_PROBABILITY; break;
+				case WID_RFO_MINIMIZE_LAKE_PROBABILITY_TEXT:   value = DEF_LAKE_REDUCE_TO_GUARANTEED_PROBABILITY; break;
+				case WID_RFO_ISLAND_PROBABILITY_TEXT:          value = DEF_LAKE_ISLAND_PROBABILITY; break;
+				case WID_RFO_SHORE_PROBABILITY_TEXT:           value = DEF_LAKE_SHORE_PROBABILITY; break;
+
+				case WID_RFO_ISLAND_MAX_SIZE_TEXT:             value = DEF_LAKE_ISLAND_MAX_SIZE; break;
+				case WID_RFO_SHORE_MAX_SIZE_TEXT:              value = DEF_LAKE_SHORE_MAX_SIZE; break;
+				default: NOT_REACHED();
+			}
+		}
+
+		this->SetWidgetDirty(this->query_widget_id);
+
+		switch (this->query_widget_id) {
+			case WID_RFO_FLOW_FOR_RIVER_TEXT:
+				_settings_newgame.game_creation.rainfall.flow_for_river = Clamp(value, MIN_FLOW_FOR_RIVER, MAX_FLOW_FOR_RIVER);
+				break;
+			case WID_RFO_LAKE_VOLUME_TEXT:
+				_settings_newgame.game_creation.rainfall.flow_per_lake_volume = Clamp(value, MIN_FLOW_PER_LAKE_VOLUME, MAX_FLOW_PER_LAKE_VOLUME);
+				break;
+			case WID_RFO_FLOW_MODIFICATIONS_TEXT:
+				_settings_newgame.game_creation.rainfall.number_of_flow_modifications = Clamp(value, MIN_NUMBER_OF_FLOW_MODIFICATIONS, MAX_NUMBER_OF_FLOW_MODIFICATIONS);
+				break;
+			case WID_RFO_WIDER_RIVERS_MULT_TEXT:
+				_settings_newgame.game_creation.rainfall.wider_rivers_multiplier = Clamp(value, MIN_WIDER_RIVERS_MULTIPLIER, MAX_WIDER_RIVERS_MULTIPLIER);
+				break;
+			case WID_RFO_WIDER_VALLEYS_MULT_TEXT:
+				_settings_newgame.game_creation.rainfall.wider_valleys_multiplier = Clamp(value, MIN_WIDER_VALLEYS_MULTIPLIER, MAX_WIDER_VALLEYS_MULTIPLIER);
+				break;
+			case WID_RFO_WIDER_VALLEYS_RANDOM_TEXT:
+				_settings_newgame.game_creation.rainfall.wider_valleys_randomness = Clamp(value, MIN_WIDER_VALLEYS_RANDOMNESS, MAX_WIDER_VALLEYS_RANDOMNESS);
+				break;
+
+			case WID_RFO_OUTFLOW_CANYON_PROBABILITY_TEXT:
+				_settings_newgame.game_creation.rainfall.lake_outflow_canyon_probability = Clamp(value, MIN_RAINFALL_PROBABILITY, MAX_RAINFALL_PROBABILITY);
+				break;
+			case WID_RFO_MINIMIZE_LAKE_PROBABILITY_TEXT:
+				_settings_newgame.game_creation.rainfall.lake_reduce_to_guaranteed_probability = Clamp(value, MIN_RAINFALL_PROBABILITY, MAX_RAINFALL_PROBABILITY);
+				break;
+			case WID_RFO_ISLAND_PROBABILITY_TEXT:
+				_settings_newgame.game_creation.rainfall.lake_island_probability = Clamp(value, MIN_RAINFALL_PROBABILITY, MAX_RAINFALL_PROBABILITY);
+				break;
+			case WID_RFO_SHORE_PROBABILITY_TEXT:
+				_settings_newgame.game_creation.rainfall.lake_shore_probability = Clamp(value, MIN_RAINFALL_PROBABILITY, MAX_RAINFALL_PROBABILITY);
+				break;
+
+			case WID_RFO_ISLAND_MAX_SIZE_TEXT:
+				_settings_newgame.game_creation.rainfall.lake_island_max_size = Clamp(value, MIN_RAINFALL_MAX_SIZE, MAX_RAINFALL_MAX_SIZE);
+				break;
+			case WID_RFO_SHORE_MAX_SIZE_TEXT:
+				_settings_newgame.game_creation.rainfall.lake_shore_max_size = Clamp(value, MIN_RAINFALL_MAX_SIZE, MAX_RAINFALL_MAX_SIZE);
 				break;
 		}
 	}
